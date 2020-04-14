32242141ea583f303f19ab9fbc660e360873fe61
lsl <2001.liu@gmail.com>
2020-04-14 11:01:06 +0900

    - add file translate nodejs(js) - change the structure of folder

---
+++ file-translation-demo/nodejs-js/config.js
@@ -0,0 +1,14 @@
+const serverConfig = {
+  protocol: 'https:',
+  hostname: 'translate.classiii.info',
+  port: 443,
+};
+
+const authConfig = {
+  accessKey: 'cad502e93e2e367aa3e3376aecf87a81f9d702213be85fe86ddf57f0c3f9b165',
+  secretKey: 'a82b5a3e48c177a02ff13b37c7e427e9f179a33f0a5e020715b455b6db012870fd58500ad877a00f239031159fff416e',
+};
+module.exports = {
+  serverConfig,
+  authConfig,
+};
